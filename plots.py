cb5383e3804f88b81cb04fa9ce51bf79b71417aa
Lucio Derin <derinlucio@yahoo.com>
2024-05-06 18:10:58 +0200

    Added residuals plots

--- plots.py
+++ plots.py
@@ -1,7 +1,7 @@
 """Script that produces the plots in ./images"""
 
 from puma import Histogram, HistogramPlot
-
+from puma.utils import get_good_colours
 import numpy as np
 
 
@@ -38,6 +38,46 @@ def plot_Lxy_comparison(
     histogram_plotter.add(MCtruth_hist, reference=True)
     histogram_plotter.add(GN2_hist, reference=False)
     histogram_plotter.add(ptracksel_hist, reference=False)
+    histogram_plotter.add(SV1_hist, reference=False)
+
+    histogram_plotter.draw()
+    filename = "images/" + filename
+    histogram_plotter.savefig(filename, dpi=200, transparent=False)
+
+def plot_residuals_comparison(
+    GN2_Lxy, perfect_tracksel_Lxy, SV1_Lxy, MCtruth_Lxy, title, filename
+):
+    colors = get_good_colours()[1:]
+    """Plot the Lxy comparison histogram"""
+    GN2_hist = Histogram(
+        (GN2_Lxy - MCtruth_Lxy), label="Fit with GN2 track selection", histtype="step", alpha=1,
+        colour=colors[0],
+    )
+    ptracksel_hist = Histogram(
+        (perfect_tracksel_Lxy-MCtruth_Lxy),
+        label="Fit with perfect track selection",
+        histtype="step",
+        alpha=1,
+        colour=colors[1]
+    )
+    SV1_hist = Histogram((SV1_Lxy - MCtruth_Lxy), label="SV1", histtype="step", alpha=1,colour=colors[2])
+
+    xrange = (-40, 40)
+    histogram_plotter = HistogramPlot(
+        ylabel="Normalized Counts",
+        xlabel=r"$fit_{L_{xy}} - MC_{L_{xy}} [mm]$",
+        logy=True,
+        bins=xrange[1] - xrange[0],
+        bins_range=xrange,
+        norm=True,
+        atlas_first_tag="Simulation Internal",
+        atlas_second_tag=r"RUN3 $t\bar{t}$",
+        figsize=(6, 5),
+        n_ratio_panels=1,
+    )
+    histogram_plotter.title = title
+    histogram_plotter.add(GN2_hist, reference=False)
+    histogram_plotter.add(ptracksel_hist, reference=True)
     histogram_plotter.add(SV1_hist, reference=False)
 
     histogram_plotter.draw()
@@ -118,6 +158,14 @@ if __name__ == "__main__":
         title=r"$L_{xy}$ comparison, b/c-jets",
         filename="LxyComparison_inclusive.jpg",
     )
+    plot_residuals_comparison(
+        GN2_Lxy,
+        perfect_tracksel_Lxy,
+        SV1_Lxy,
+        MCtruth_Lxy,
+        title=r"Residuals comparison, b/c-jets",
+        filename="ResidualsComparison_inclusive.jpg",
+    )
     plot_chi2_comparison(
         perfect_tracksel_chi2,
         GN2_chi2,
@@ -142,6 +190,14 @@ if __name__ == "__main__":
         title=r"$L_{xy}$ comparison, c-jets only",
         filename="LxyComparison_cjets.jpg",
     )
+    plot_residuals_comparison(
+        GN2_Lxy_cjets,
+        perfect_tracksel_Lxy_cjets,
+        SV1_Lxy_cjets,
+        MCtruth_Lxy_cjets,
+        title=r"Residuals comparison, c-jets only",
+        filename="ResidualsComparison_cjets.jpg",
+    )
     plot_chi2_comparison(
         perfect_tracksel_chi2_cjets,
         GN2_chi2_cjets,
@@ -165,6 +221,14 @@ if __name__ == "__main__":
         MCtruth_Lxy_bjets,
         title=r"$L_{xy}$ comparison, c-jets only",
         filename="LxyComparison_bjets.jpg",
+    )
+    plot_residuals_comparison(
+        GN2_Lxy_bjets,
+        perfect_tracksel_Lxy_bjets,
+        SV1_Lxy_bjets,
+        MCtruth_Lxy_bjets,
+        title=r"Residuals comparison, c-jets only",
+        filename="ResidualsComparison_bjets.jpg",
     )
     plot_chi2_comparison(
         perfect_tracksel_chi2_bjets,
